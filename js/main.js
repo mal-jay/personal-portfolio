b2d69e49538638fd97edd5f21223a96e55b404ab
Malaka Mithila <malaka.mithila@gmail.com>
2022-03-18 18:28:08 +1000

    Change Color, add portfolio page

--- js/main.js
+++ js/main.js
@@ -10,14 +10,24 @@
 //     }
 // })
 
-
-// Skill bar
+// Skill bar ==================================
 $('.skill').waypoint(function (){
     $('.progress .progress-bar').each(function (){
        $(this).css("width", $(this).attr("aria-valuenow") + '%'); 
     });
 }, {offset: '80%'});
 
+ // Portfolio isotope and filter ==============================
+ var portfolioIsotope = $('.portfolio-container').isotope({
+    itemSelector: '.portfolio-item',
+    layoutMode: 'fitRows'
+});
+$('#portfolio-flters li').on('click', function () {
+    $("#portfolio-flters li").removeClass('active');
+    $(this).addClass('active');
+
+    portfolioIsotope.isotope({filter: $(this).data('filter')});
+});
 
 
 })(jQuery);
